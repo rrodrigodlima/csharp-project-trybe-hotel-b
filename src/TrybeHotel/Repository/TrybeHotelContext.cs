79c9312cb9e65eb350d03852f45215e5a5a0fe50
rrodrigodlima <rdrigo019@gmail.com>
2023-10-25 04:27:07 -0300

    test context

--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -4,11 +4,11 @@ using TrybeHotel.Models;
 namespace TrybeHotel.Repository;
 public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
-    public DbSet<City> Cities { get; set; }
-    public DbSet<Hotel> Hotels { get; set; }
-    public DbSet<Room> Rooms { get; set; }
-    public DbSet<User> Users { get; set; } // Adicione esta linha
-    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<City>? Cities { get; set; }
+    public DbSet<Hotel>? Hotels { get; set; }
+    public DbSet<Room>? Rooms { get; set; }
+    public DbSet<User>? Users { get; set; } 
+    public DbSet<Booking>? Bookings { get; set; }
     public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options)
     {
         Seeder.SeedUserAdmin(this);
