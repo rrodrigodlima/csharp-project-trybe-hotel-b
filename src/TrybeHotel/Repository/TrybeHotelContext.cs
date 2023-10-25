3ab948db8b7ae336116703bba9f0e484dcae903e
rrodrigodlima <rdrigo019@gmail.com>
2023-10-25 16:15:36 -0300

    test a diferent context

--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -4,11 +4,11 @@ using TrybeHotel.Models;
 namespace TrybeHotel.Repository;
 public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
-    public DbSet<City> Cities { get; set; } = null;
-    public DbSet<Hotel> Hotels { get; set; } = null;
-    public DbSet<Room> Rooms { get; set; } = null;
-    public DbSet<User> Users { get; set; } = null;
-    public DbSet<Booking> Bookings { get; set; } = null;
+    public DbSet<City>? Cities { get; set; }
+    public DbSet<Hotel>? Hotels { get; set; }
+    public DbSet<Room>? Rooms { get; set; }
+    public DbSet<User>? Users { get; set; }
+    public DbSet<Booking>? Bookings { get; set; }
     public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options)
     {
         Seeder.SeedUserAdmin(this);
@@ -23,5 +23,25 @@ public class TrybeHotelContext : DbContext, ITrybeHotelContext
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {}
+    {
+        modelBuilder.Entity<City>()
+                    .HasMany(c => c.Hotels)
+                    .WithOne(h => h.City)
+                    .HasForeignKey(h => h.CityId);
+
+        modelBuilder.Entity<Hotel>()
+                    .HasMany(h => h.Rooms)
+                    .WithOne(r => r.Hotel)
+                    .HasForeignKey(r => r.HotelId);
+
+        modelBuilder.Entity<User>()
+                    .HasMany(u => u.Bookings)
+                    .WithOne(b => b.User)
+                    .HasForeignKey(b => b.UserId);
+
+        modelBuilder.Entity<Room>()
+                    .HasMany(r => r.Bookings)
+                    .WithOne(b => b.Room)
+                    .HasForeignKey(b => b.RoomId);
+    }
 }
